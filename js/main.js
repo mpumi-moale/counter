159f1090ca3c4e82a940c8fde9579190e88374e7
mmoale021 <mmoale021@student.wethinkcode.co.za>
2022-11-24 14:43:24 +0200

    js update

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ var CountDownDate = new Date("Jan 1, 2023 00:00:00").getTime(); // when countdow
 
 var counting = setInterval(function(){
     var now = new Date().getTime();
-    var distance = CountDownDate - now;
+    var time = CountDownDate - now;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var days = Math.floor(time / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((time % (1000 * 60)) / 1000);
 
     document.getElementById("days").innerHTML = days;
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 
-    if(distance < 0) {
+    if(time < 0) {
         clearInterval(counting);
         document.getElementById("days").innerHTML = "00";
         document.getElementById("hours").innerHTML =  "00";
